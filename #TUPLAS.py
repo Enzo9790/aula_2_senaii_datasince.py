abfb6aed1bad3e192a0f1125ad084ea147183f3d
Kusma <99890071+Enzo9790@users.noreply.github.com>
2025-10-24 17:51:43 -0300

    Add files via upload

---
+++ #TUPLAS.py
@@ -0,0 +1,26 @@
+#TUPLAS
+
+
+# Imutavel em essencia 
+# porem é possivek garregar dado com +=()
+# acssamos os indices com o colchetes
+
+
+
+lista  =  [0,30,0,30]
+
+
+tupla  = (10,2,3)
+t =  1,2,3,4,5,6
+
+
+tup =  tuple(range(1,11))
+print(tup)
+
+
+tup += (10,30,10) 
+
+
+# tupla.append(100)
+print(tup)
+
